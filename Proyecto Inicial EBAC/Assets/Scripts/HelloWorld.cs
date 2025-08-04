5f4961161d41ee21d787f082fac2704b400c0086
FrOniishi <franco.romero.19999@gmail.com>
2025-08-04 19:51:45 -0300

    Script con funciones
    Se agrego un Script con funciones del game loop de Unity

--- Proyecto Inicial EBAC/Assets/Scripts/HelloWorld.cs
+++ Proyecto Inicial EBAC/Assets/Scripts/HelloWorld.cs
@@ -5,12 +5,30 @@ public class HelloWorld : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Debug.Log("Hello, World!");
+        Debug.Log("Hello World From Start");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log("Hello World From Update");
+
+    }
+
+    private void Awake()
+    {
+        Debug.Log("Hello World From Awake");
+    }
+
+    private void FixedUpdate()
+    {
+        Debug.Log("Hello World From Update Fixed");
+
+    }
+
+    private void LateUpdate()
+    {
+        Debug.Log("Hello World From LateUpdate");
+
     }
 }
