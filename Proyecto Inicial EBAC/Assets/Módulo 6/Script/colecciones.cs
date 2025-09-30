4bf8a8498f3f80068bfb7cbdfa92632b677a84bd
FrOniishi <franco.romero.19999@gmail.com>
2025-09-30 15:16:48 -0300

    Update colecciones.cs
    Actualización de la actividad de estructuras de datos

--- Proyecto Inicial EBAC/Assets/Módulo 6/Script/colecciones.cs
+++ Proyecto Inicial EBAC/Assets/Módulo 6/Script/colecciones.cs
@@ -4,60 +4,105 @@ using UnityEngine;
 
 public class colecciones : MonoBehaviour
 {
-    List<int> miFuncion = new List<int>();
+    int parametro1 = 5;
+    int parametro2 = 10;
+    int parametro3 = 20;
 
-    List<string> miFuncion2 = new List<string> { "Hola", "que", "tal", "todo", "Hola", "tal", "Hola" };
-
-    Stack<string> miFuncion5 = new Stack<string>();
+    List<string> palabrasAleatorias = new List<string> { "Hola", "que", "tal", "todo", "Hola", "tal", "Hola" };
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        for (int i = 0; i < 3; i++)
+        lista_ordenada();
+        HashSet<string> sinRepetidos = EliminarRepetidos(palabrasAleatorias);
+
+        foreach (string palabra in sinRepetidos)
         {
-            miFuncion.Add(Random.Range(1,20));
+            Debug.Log(palabra);
         }
-        miFuncion.Sort();
-        miFuncion.Reverse();
-        foreach (int numero in miFuncion)
+
+        string[] elementosPila = { "Elemento1", "Elemento2", "Elemento3", "Elemento4" };
+        ProcesarEstructuras(elementosPila);
+    }
+
+    void lista_ordenada()
+    {
+        List<int> resultado = listaNumeros(parametro1, parametro2, parametro3);
+        List<int> ordenada = OrdenarDescendente(resultado);
+
+        foreach (int numero in ordenada)
         {
             Debug.Log(numero);
         }
-
-        HashSet<string> miFuncion3 = new HashSet<string>(miFuncion2);
-
-        foreach (string palabra in miFuncion3)
-        {
-            Debug.Log(palabra);
-        }
-
-        miFuncion5.Push("Primero");
-        miFuncion5.Push("Segundo");
-        miFuncion5.Push("Tercero");
-        miFuncion5.Push("Cuarto");
-        miFuncion5.Push("Quinto");
-
-        miFuncion5.Pop();
-
-        foreach (string palabra in miFuncion5)
-        {
-            Debug.Log(palabra);
-        }
-
-        Queue<string> miFuncion4 = new Queue<string>(miFuncion5);
-
-        Debug.Log("El primer elemento es: " + miFuncion4.Peek());
-        miFuncion4.Dequeue();
-
-        foreach (string palabra in miFuncion4)
-        {
-            Debug.Log(palabra);
-        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public List<int> listaNumeros(int tamaño, int rangoInferior, int rangoSuperior)
     {
-        
+        List<int> listaAleatoria = new List<int>();
+
+        for (int i = 0; i < tamaño; i++)
+        {
+            listaAleatoria.Add(Random.Range(rangoInferior, rangoSuperior));
+        }
+
+        return listaAleatoria;
     }
+
+    public List<int> OrdenarDescendente(List<int> entrada)
+    {
+        List<int> copia = new List<int>(entrada);
+
+        copia.Sort();
+        copia.Reverse();
+
+        return copia;
+    }
+
+    public HashSet<string> EliminarRepetidos(List<string> entrada)
+    {
+        HashSet<string> resultado = new HashSet<string>();
+
+        foreach (string elemento in entrada)
+        {
+            resultado.Add(elemento);
+        }
+
+        return resultado;
+    }
+
+    public void ProcesarEstructuras(string[] elementos)
+    {
+        Stack<string> pila = new Stack<string>();
+        foreach (string elemento in elementos)
+        {
+            pila.Push(elemento);
+        }
+
+        Queue<string> cola = new Queue<string>();
+
+        Stack<string> copiaPila = new Stack<string>(pila);
+        foreach (string _ in copiaPila)
+        {
+            string cima = pila.Peek();
+            Debug.Log("Peek de pila: " + cima);
+
+            cola.Enqueue(cima);
+            Debug.Log("Enqueue a cola: " + cima);
+
+            pila.Pop();
+            Debug.Log("Pop de pila: " + cima);
+        }
+
+        int cantidad = cola.Count;
+        for (int i = 0; i < cantidad; i++)
+        {
+            string frente = cola.Peek();
+            Debug.Log("Peek de cola: " + frente);
+
+            cola.Dequeue();
+            Debug.Log("Dequeue de cola: " + frente);
+        }
+    }
+
 }
+
